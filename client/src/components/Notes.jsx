7f21313153a2a8405c7d8b364651e7065c271352
Debid Magar <linkwithdm@gmail.com>
2021-07-31 20:19:48 +0545

    fetch error fixed

--- client/src/components/Notes.jsx
+++ client/src/components/Notes.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:3005";
 
 function Notes() {
   const [notes, setNotes] = useState([
@@ -9,19 +11,27 @@ function Notes() {
   ]);
 
   useEffect(() => {
-    fetch("/notes/")
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
+    axios
+      .get("/notes")
+      .then((response) => {
+        setNotes(response.data);
       })
-      .then((jsonRes) => setNotes(jsonRes));
+      .catch((error) => {
+        console.log(error);
+      });
+    // fetch("/notes/")
+    //   .then((res) => {
+    //     if (res.ok) {
+    //       return res.json();
+    //     }
+    //   })
+    //   .then((jsonRes) => setNotes(jsonRes));
   });
   return (
     <div className="container">
       <h2>Notes page</h2>
-      {notes.map((note) => (
-        <div>
+      {notes.map((note, index) => (
+        <div key={index}>
           <h2> {note.title}</h2>
           <p> {note.content}</p>
         </div>
